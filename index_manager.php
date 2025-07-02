f16062a2e9b625075027fd2cc169b072e18c83fc
Maeva-Merour <merour.maeva@gmail.com>
2025-07-02 11:30:46 +0200

    final class

--- index_manager.php
+++ index_manager.php
@@ -21,7 +21,7 @@ class Employe
     // C’est utile pour changer ou adapter le comportement d’une méthode dans une classe spécialisée (comme un Manager qui se comporte différemment d’un Employe).
     public function travailler()
     {
-        echo $this->nom . " travaille";
+        echo $this->nom . " travaille. <br>";
     }
     public function afficherRole()
 
@@ -37,6 +37,11 @@ class Employe
     {
         echo "Je suis un employé. <br>";
     }
+
+        public function afficherInfos()
+    {
+        echo "Gagne " . $this->salaire . " &euro;. <br>";
+    }
 }
 
 class Manager extends Employe
@@ -45,13 +50,64 @@ class Manager extends Employe
 // Donc, un objet de type Manager pourra utiliser la méthode travailler() et accéder à $nom.
 // Combines le comportement de la classe de base et celui du manager.
 {
+    private array $manage = [];
+
     public function afficherRole()
     {
         // :: signifie hériter de - Cela appelle la méthode afficherRole() de la classe Employe, donc ça va exécuter echo $this->nom . " travaille";.
         parent::afficherRole();
-        echo "Je suis un manager.";
+        echo "Je suis un manager. <br>";
+    }
+
+    public function ajouterEquipier(Employe $equipier)
+    {
+        $this->manage[] = $equipier;
+    }
+    public function afficherEquipe()
+    {
+        echo $this->nom . " manage " . count($this->manage) . " employés.";
     }
 }
 
+final class Directeur extends Manager
+// final permet de faire de directeur la dernière classe d'héritage (impossible d'en créer une au-dessus - c'est le dernier maillon de notre chaîne)
+{
+    private array $supervision = [];
+
+    public function ajouterManager(Manager $manager)
+    {
+        $this->supervision[] = $manager;
+    }
+
+    public function afficherInfos()
+    {
+        echo $this->nom. " supervise " . count($this->supervision) . " managers.";
+    }
+}
+
+$employeOne = new Employe("Paul", 1600);
+$employeOne->travailler();
+$employeOne->afficherRole();
+$employeOne->afficherInfos();
+
+$employeTwo = new Employe("Gaïa", 1590);
+$employeTwo->travailler();
+$employeTwo->afficherInfos();
+$employeTwo->afficherRole();
+
+$employeThree = new Employe("Léa", 1400);
+$employeThree->travailler();
+$employeThree->afficherInfos();
+$employeThree->afficherRole();
+
 $managerOne = new Manager("Jean", 2100);
+$managerOne->travailler();
 $managerOne->afficherRole();
+$managerOne->ajouterEquipier($employeOne);
+$managerOne->ajouterEquipier($employeTwo);
+$managerOne->ajouterEquipier($employeThree);
+$managerOne->afficherEquipe();
+
+$directorOne = new Directeur("Big Boss", 50000);
+$directorOne->ajouterManager($managerOne);
+$directorOne->afficherInfos();
