6e845cd84509ab6f88c01c59a1e19dbf8689064f
Maeva-Merour <merour.maeva@gmail.com>
2025-07-03 09:34:55 +0200

    trait

---
+++ index_trait.php
@@ -0,0 +1,35 @@
+<?php
+
+// un trait est un moyen de réutiliser du code dans plusieurs classes, sans utiliser l’héritage.
+// Quand plusieurs classes ont des fonctions communes, mais qu’elles n’ont pas de lien direct entre elles (donc pas possible d'utiliser l’héritage), 
+// on peut mettre ce code commun dans un trait.
+// Un trait est comme un bloc de fonctions réutilisable.
+// On peut utiliser plusieurs traits dans une classe.
+// Il évite la duplication de code et contourne les limites de l’héritage simple (PHP ne permet pas d’héritage multiple entre classes).
+
+trait Logger{
+    public function log(string $msg):void{
+        echo "Log : ". $msg . " <br>";
+    }
+}
+
+trait Mailer{
+    public function sendMail(string $dest, string $msg):void{
+        echo "Mail envoyé à ". $dest. " : ". $msg . " <br>";
+    }
+}
+
+class NotificationService{
+    
+    // on appelle un trait à l’intérieur d’une classe en utilisant le mot-clé use.
+    use Logger, Mailer;
+
+    public function notifierClient(){
+        $this->log("Vous êtes connecté.");
+        $this->sendMail("contact@contact.fr","Salut !");
+    }
+
+}
+
+$notif = new NotificationService();
+$notif->notifierClient();
